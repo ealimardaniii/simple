a82606c029b15e8c97cd641ac94fa19cf9d63b55
Erfan Alimardani <mo.erfana6505@yahoo.com>
2024-08-12 03:13:14 +0330

    feat: init styles

---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const COLORS = {
+  white: '#ffffff',
+  black: '#000',
+  mySin: '#fbaf23',
+  shingleFawn: '#704f37',
+};
+
+export type Color = keyof typeof COLORS;
---
+++ src/styles/typography.ts
@@ -0,0 +1,26 @@
+export const FONT_FAMILY = {
+  decondorBold: 'decondor-bold',
+};
+
+export const FONT_SIZE = {
+  /**
+   * `12`
+   */
+  x12: 12,
+  /**
+   * `14`
+   */
+  x14: 14,
+  /**
+   * `16`
+   */
+  x16: 16,
+  /**
+   * `20`
+   */
+  x20: 20,
+  /**
+   * `24`
+   */
+  x24: 24,
+};
